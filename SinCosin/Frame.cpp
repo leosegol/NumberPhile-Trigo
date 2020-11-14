83a300ed6973227eaf4d45a751011695fa0d6845
Leo <leosegol@gmail.com>
2020-11-14 18:27:45 +0200

    Draw bitch

--- SinCosin/Frame.cpp
+++ SinCosin/Frame.cpp
@@ -1,6 +1,7 @@
 #include"Line.h"
 #include"Circle.h"
 #include"Frame.h"
+#include<SDL.h>
 #include<iostream>
 
 constexpr auto length = 3;
@@ -20,19 +21,42 @@ Point* DataFrame(Circle& c, int num, Line* lines)
 
 int main()
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* w = SDL_CreateWindow("SinProject", 0, 0, 1000, 700, SDL_WINDOW_SHOWN);
+	SDL_Renderer* render = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
 	Circle c = Circle(10);
 	Line lines[length];
 	std::cout << sizeof lines / sizeof(Line) << std::endl;
 	for (int i = 0; i < sizeof lines/ sizeof(Line); i++)
 		lines[i] = Line(360 / sizeof lines, i);
 	Point* points;
-		c.SetDegrree(0);
+	SDL_Event event;
+	for(int j = 0;;j++)
+	{ 
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				exit(1);
+			}
+		}
+		SDL_SetRenderDrawColor(render, 0, 0, 0, 255);
+		SDL_RenderClear(render);
+
+		SDL_SetRenderDrawColor(render, 255, 255, 0, 255);
+
+		c.SetDegrree(1*j);
 		std::cout << c.x << ", " << c.y << std::endl;
 		points = DataFrame(c, sizeof lines/ sizeof(Line), lines);
 		// we need to draw points
-			for (int i = 0; i < sizeof lines / sizeof(Line); i++)
-			{
-				std::cout << points[i].x << ", " << points[i].y << std::endl;
-			}
+
+		for (int i = 0; i < sizeof lines / sizeof(Line); i++)
+		{
+			//std::cout << points[i].x << ", " << points[i].y << std::endl;
+			SDL_Rect rect = { points[i].x, points[i].y, points[i].x+10, points[i].y+10 };
+		}
+	
+	}
 	
 }
